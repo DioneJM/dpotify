6371610a7f59b651a9742a71e98f8ba5b9ad45c5
Dione <dionemorales@outlook.com>
2022-04-17 18:14:57 +1000

    added skeleton for player layout component

---
+++ components/PlayerLayout.tsx
@@ -0,0 +1,13 @@
+import {Box} from "@chakra-ui/layout";
+
+export interface PlayerLayoutProps {
+}
+
+const PlayerLayout = ({children}) => {
+    return <Box>
+      PlayerLayout
+        {children}
+    </Box>
+}
+
+export default PlayerLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {ChakraProvider} from "@chakra-ui/react";
+import PlayerLayout from "../components/PlayerLayout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return <ChakraProvider>
+        <PlayerLayout>
+            <Component {...pageProps} />
+        </PlayerLayout>
+    </ChakraProvider>
 }
 
 export default MyApp
