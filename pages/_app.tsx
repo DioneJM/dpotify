5ffdfc0fc6fbf0bdfb488f9ed5b655af94e8cb2d
Dione <dionemorales@outlook.com>
2022-04-21 22:54:33 +1000

    added theme for chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,43 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
-import {ChakraProvider} from "@chakra-ui/react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import PlayerLayout from "../components/PlayerLayout";
 
-const MyApp = ({Component, pageProps}: AppProps) => {
-    return <ChakraProvider>
-        <PlayerLayout>
-            <Component {...pageProps} />
-        </PlayerLayout>
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
     </ChakraProvider>
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
