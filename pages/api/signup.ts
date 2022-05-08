da3e5d6b65aba18b4b64fae7927fb820fc604371
Dione <dionemorales@outlook.com>
2022-05-08 13:48:18 +1000

    create signup api

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,8 +1,48 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import prisma from "../../lib/prisma";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {};
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (e) {
+    console.log("failed to create user: ", e);
+    res.status(401);
+    res.json({ error: "Signup Failed" });
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      signedOn: Date.now(),
+    },
+    "secret_hello",
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("TRAX_ACCESS_TOKEN ", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60, // 8 hours
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  res.json(user);
+};
