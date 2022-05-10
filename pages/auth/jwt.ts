d95bbe44a4d7d2790ea4a306637acf8f5aeec634
Dione <dionemorales@outlook.com>
2022-05-10 17:16:32 +1000

    extract hard coded number into variable

--- pages/auth/jwt.ts
+++ pages/auth/jwt.ts
@@ -15,12 +15,13 @@ export const generateSignedJwtFor = (user: User) => {
   );
 };
 
+const eightHoursInMs = 8 * 60 * 60;
 export const setJwtCookie = (token: string, response: NextApiResponse) => {
   response.setHeader(
     "Set-Cookie",
     cookie.serialize("TRAX_ACCESS_TOKEN ", token, {
       httpOnly: true,
-      maxAge: 8 * 60 * 60, // 8 hours
+      maxAge: eightHoursInMs,
       path: "/",
       sameSite: "lax",
       secure: process.env.NODE_ENV === "production",
