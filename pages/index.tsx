7d38dae96deebebcff430ac0d0a663d292e0d934
Dione <dionemorales@outlook.com>
2022-05-17 18:21:46 +1000

    added artists section to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,57 @@
-import type { NextPage } from "next";
-import GradientLayout from "../components/GradientLayout";
 import prisma from "../lib/prisma";
+import GradientLayout from "../components/GradientLayout";
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { Artist } from "@prisma/client";
+import { FC } from "react";
 
-const Home: NextPage = ({ artists }) => {
+interface ServerSideProps {
+  artists: Artist[];
+}
+
+const Home: FC<ServerSideProps> = ({ artists }) => {
+  const color = "purple";
   return (
     <GradientLayout
-      color="green"
+      color={color}
       title="hi title"
       subtitle="the best album in the game"
       description="some album I made"
       imageSrc="https://geekhack.org/index.php?action=dlattach;attach=89492;type=avatar"
       roundImage
     >
-      <div>Content</div>
+      <Box color={"white"} paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex justify={"center"}>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%" height={"20%"}>
+              <Box
+                bg={`${color}.900`}
+                borderRadius="4px"
+                padding="15px"
+                width="100%"
+              >
+                <Image src="https://placedog.net/300/300" borderRadius="100%" />
+                <Box marginTop="10px">
+                  <Text>{artist.name}</Text>
+                  <Text>{"Artist"}</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (): Promise<{
+  props: ServerSideProps;
+}> => {
   const artists = await prisma.artist.findMany({});
 
   return {
