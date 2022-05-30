f5644e65dd9787c828729e439be36edfd06fe21d
Dione <dionemorales@outlook.com>
2022-05-30 18:36:18 +1000

    fix middleware signin logic

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const signedInPages = ["/", "/playlist", "/library"];
+const signedOutPages = ["/signin"];
 
 // We don't take in a response object because this will be run on the edge similar to a web worker
 export default function middleware(req: NextRequest) {
-  if (signedInPages.includes(req.nextUrl.pathname)) {
+  const pageRequiresSignIn = !signedOutPages.some((route) =>
+    req.nextUrl.pathname.includes(route)
+  );
+  if (pageRequiresSignIn) {
     const token = req.cookies.TRAX_ACCESS_TOKEN;
 
     if (!token) {
