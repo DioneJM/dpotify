1174fd397515514b6b770799ff4ff983e1c1df86
Dione <dionemorales@outlook.com>
2022-05-30 18:38:39 +1000

    add signup page as signedOutPages

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const signedOutPages = ["/signin"];
+const signedOutPages = ["/signin", "/signup"];
 
 // We don't take in a response object because this will be run on the edge similar to a web worker
 export default function middleware(req: NextRequest) {
