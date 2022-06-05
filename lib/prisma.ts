ee726ea073a82685cd91e98e5c10cb5abcb6534e
Dione <dionemorales@outlook.com>
2022-06-05 13:56:30 +1000

    make singleton version of prisma to prevent too many connection errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
+let prisma: PrismaClient;
+if (process.env.NODE_ENV !== "production") {
+  console.log("Creating prisma client for dev environment");
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      debug: true,
+    });
+  }
+  prisma = global.prisma;
+} else {
+  prisma = new PrismaClient();
+}
 export default prisma;
