73ee11a56e08cc783b59e808faf071d747465dba
Dione <dionemorales@outlook.com>
2022-06-07 21:06:05 +1000

    add share paths to public pages

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const publicPages = ["/signin", "/signup", "/public"];
+const publicPages = ["/signin", "/signup", "/public", "/share"];
 
 // We don't take in a response object because this will be run on the edge similar to a web worker
 export default function middleware(req: NextRequest) {
