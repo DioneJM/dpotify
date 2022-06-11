682d150644993955c2b6d19b599e99aece333eaa
Dione <dionemorales@outlook.com>
2022-06-11 19:07:03 +1000

    added custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default () => (
+  <Html>
+    <Head />
+    <body
+      style={{
+        minHeight: "100vh",
+        overflow: "none",
+      }}
+    >
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
